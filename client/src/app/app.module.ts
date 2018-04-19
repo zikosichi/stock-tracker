21971ce1e0bd948e491cff4d3325f37f6894b6ac
Zviad Sichinava <zviad.sichinava@devfactory.com>
2018-04-19 19:08:20 +0200

    chore: created new stock component & stock service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { NewStockComponent } from './components/new-stock/new-stock.component';
 
+// Services
+import { StockService } from './services/stock.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewStockComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [StockService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/components/new-stock/new-stock.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-stock',
+  templateUrl: './new-stock.component.html',
+  styleUrls: ['./new-stock.component.scss']
+})
+export class NewStockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
